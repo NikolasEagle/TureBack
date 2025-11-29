711967d7b101c01d2d8b160e7942e4c802c112bb
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-11-29 21:23:24 +0400

    Добавление функций получения текущей даты, имени хоста и функции генерации статуса сообщения

--- logging/logging.sh
+++ logging/logging.sh
@@ -4,4 +4,16 @@ get_date() {
     date +"%b %d %H:%M:%S";
 }
 
-export -f get_date
+get_hostname() {
+    hostname
+}
+
+get_message() {
+    if $1; then
+        echo "[INFO]:"
+    else
+        echo "[ERROR]:"
+    fi
+}
+
+export -f get_date get_hostname get_message
