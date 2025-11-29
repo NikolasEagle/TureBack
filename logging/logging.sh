6e5e71df6ad367d8284d5052a030a269b1f6aa8f
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-11-29 22:42:07 +0400

    Добавление инструкции выхода из скрипта с ошибкой

--- logging/logging.sh
+++ logging/logging.sh
@@ -17,6 +17,7 @@ logging() {
 
     if [[ -n $err ]]; then
         echo "$(get_date) $(get_hostname) [ERROR]: $err" >> $logs_path
+        set -e
     elif [[ -z $err && $backup_completed ]]; then
         echo "$(get_date) $(get_hostname) [INFO]: Success!" >> $logs_path
     fi    
