8ea848ce00ba7661173b3210a824eadb291b6d7f
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-12-01 20:09:44 +0400

    Добавление функционала удаления старых копий

---
+++ backuping/backuping.sh
@@ -0,0 +1,15 @@
+backuping() {
+    local backup_path=$1
+    local count_backup_files=$2
+    local disk=$3
+
+    # Подсчет количества копий в целевой директории
+
+    count_backup_files_in_folder=$(find $backup_path -name ".img.lzo" | wc -l)
+
+    # Удаление старой копии при превышении количества копий в папке необходимого числа
+
+    if [[ $count_backup_files_in_folder -ge $count_backup_files ]]; then
+        ls -t | tail -n 1 | xargs rm
+    fi
+}
