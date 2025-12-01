ff643b9fd67310d8ac17d335a052effbc04f3d61
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-12-01 20:51:19 +0400

    Создание исполняемого файла программы

--- main.sh
+++ main.sh
@@ -2,34 +2,4 @@
 
 source $(dirname $0)/funcs/*
 
-# Чтение флагов
 
-usage() {
-    echo "Usage: $0 [-o backup_path] [-n count_backup_files] [-d disk]"
-    exit 1
-}
-
-while getopts ":o:n:d:" opt; do
-    case $opt in
-        o)
-            echo "Backup Path: $OPTARG"
-            ;;
-        n)
-            echo "Count Backup Files: $OPTARG"
-            ;;
-        d)
-            echo "Disk: $OPTARG"
-            ;;
-        c)
-            echo "Compression ratio: $OPTARG"
-            ;;            
-        \?)
-            echo "Error: Invalid option -$OPTARG" >&2
-            usage
-            ;;
-        :)
-            echo "Error: Option -$OPTARG requires an argument." >&2
-            usage
-            ;;
-    esac
-done
