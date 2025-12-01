a1b719d4b7049afd80f999a870eca17f5e125a88
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-12-01 17:40:47 +0400

    Удаление функции get_hostname

--- logging/logging.sh
+++ logging/logging.sh
@@ -4,10 +4,6 @@ get_date() {
     date +"%b %d %H:%M:%S";
 }
 
-get_hostname() {
-    hostname
-}
-
 logging() {
     local command=$1
     local logs_path=$2
@@ -16,10 +12,10 @@ logging() {
     local err=$($command 2>&1)
 
     if [[ -n $err ]]; then
-        echo "$(get_date) $(get_hostname) [ERROR]: $err" >> $logs_path
+        echo "$(get_date) $(hostname) [ERROR]: $err" >> $logs_path
         exit 1
     elif [[ -z $err && $backup_completed ]]; then
-        echo "$(get_date) $(get_hostname) [INFO]: Success!" >> $logs_path
+        echo "$(get_date) $(hostname) [INFO]: Success!" >> $logs_path
     fi    
 }
 
