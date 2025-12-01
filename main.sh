fff5fac88bea05d3d64096a28d4ba03ecbe1c5ce
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-12-01 23:11:21 +0400

    Импортирование и вызов функций резервного копирования

--- main.sh
+++ main.sh
@@ -1,5 +1,15 @@
 # Импорт функций
 
-source $(dirname $0)/funcs/*
+source $(dirname $0)/funcs/backuping.sh
 
+main() {
+    local backup_path=$1
+    local count_backup_files=$2
+    local disk=$3
+    local compression_ratio=$4
+    local logs_path="$backup_path/tureback.log"
 
+    backuping $backup_path $count_backup_files $disk $compression_ratio $logs_path
+}
+
+export -f main
