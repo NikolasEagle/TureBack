6e8799ccf49db9ed07a08ae4db8bc73776bc796e
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-12-02 19:09:41 +0400

    Реализация функции проверки установленного пакета в разных дистрибутивах

---
+++ funcs/check_package.sh
@@ -0,0 +1,66 @@
+check_package() {
+    local package=$1
+
+    # Текст ошибки при невозможности установки пакета
+
+    local err="Error: Packet $package can't be installed"
+    
+    # Проверяем, какая система
+
+    if command -v apt >/dev/null 2>&1; then
+        # Debian/Ubuntu
+        if ! $(apt list --installed $package > /dev/null 2>&1); then
+            apt install $package -y
+            if [ $? -ne 0 ]; then
+                logging "ERROR" $err $logs_path
+                exit 1
+            fi
+        fi
+        
+    elif command -v dnf >/dev/null 2>&1; then
+        # RHEL/CentOS/Fedora
+        if ! $(dnf list installed $package >/dev/null 2>&1); then
+            dnf install -y $package
+            if [ $? -ne 0 ]; then
+                logging "ERROR" $err $logs_path
+                exit 1
+            fi
+        fi
+        
+    elif command -v pacman >/dev/null 2>&1; then
+        # Arch
+        if ! $(pacman -Qi $package >/dev/null 2>&1); then
+            pacman -S --noconfirm $package
+            if [ $? -ne 0 ]; then
+                logging "ERROR" $err $logs_path
+                exit 1
+            fi
+        fi
+        
+    elif command -v zypper >/dev/null 2>&1; then
+        # OpenSUSE
+        if ! $(zypper se -i $package >/dev/null 2>&1); then
+            zypper install -y $package
+            if [ $? -ne 0 ]; then
+                logging "ERROR" $err $logs_path
+                exit 1
+            fi
+        fi
+
+    elif command -v apk >/dev/null 2>&1; then
+        # Alpine
+         if ! $(apk info --installed lz4 > /dev/null 2>&1); then
+            apk add $package
+            if [ $? -ne 0 ]; then
+                logging "ERROR" $err $logs_path
+                exit 1
+            fi
+        fi
+        
+    else
+        logging "ERROR" "$err. Unable to determine package manager" $logs_path
+        exit 1
+    fi
+}
+
+export -f check_package
