2cf86bd0d4891cbd7ced2dfc6b2d9a83859cd262
Nickolas Eagle <eagle.dev.stack@gmail.com>
2025-12-02 10:57:05 +0400

    Добавление аргумента для пути журнала

--- funcs/logging.sh
+++ funcs/logging.sh
@@ -5,6 +5,7 @@ source $(dirname $0)/funcs/date.sh
 logging() {
     local status=$1
     local message=$2
+    local logs_path=$3
 
     echo "$(get_date "journal") $(hostname) [$status]: $message" | tee -a $logs_path
 
